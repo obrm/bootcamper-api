4b504c2141e3eed94578f9a0d4a1801c04324af3
Ori Baram <obrm770@gmail.com>
2023-03-09 11:11:42 +0200

    Advanced queries

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -4,9 +4,14 @@ import {
   getBootcamp,
   createBootcamp,
   updateBootcamp,
-  deleteBootcamp
+  deleteBootcamp,
+  getBootcampsInRadius
 } from '../controllers/bootcamps.js';
 const router = express.Router();
+
+router
+  .route('/radius/:zipcode/:distance')
+  .get(getBootcampsInRadius);
 
 router
   .route('/')
