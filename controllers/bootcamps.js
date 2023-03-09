51bbb8cf5ae45d78bffa8f4ab366afb4cbc6c7c2
Ori Baram <obrm770@gmail.com>
2023-03-09 11:11:29 +0200

    Advanced queries

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,4 @@
+import geocoder from '../utils/geocoder.js';
 import Bootcamp from '../models/Bootcamp.js';
 import ErrorResponse from '../utils/errorResponse.js';
 import asyncHandler from '../middleware/async.js';
@@ -5,25 +6,84 @@ import asyncHandler from '../middleware/async.js';
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-export const getBootcamps = asyncHandler(async (req, res, next) => {  
-    const bootcamps = await Bootcamp.find();
+export const getBootcamps = asyncHandler(async (req, res, next) => {
+  // Copy req.query
+  const reqQuery = { ...req.query };
 
-    res.status(200).json({
-      success: true,
-      count: bootcamps.length,
-      data: bootcamps,
-    });
+  // Fields to exclude
+  const removeFields = ['select', 'sort', 'limit', 'page'];
+
+  // Loop over removeFields and delete them from reqQuery
+  removeFields.forEach((field) => delete reqQuery[field]);
+
+  // Create query string
+  let queryStr = JSON.stringify(reqQuery);
+
+  // Create operators ($gt, $gte, $lt, $lte, etc)
+  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+  // Finding resource
+  const query = Bootcamp.find(JSON.parse(queryStr));
+
+  // Select fields
+  if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+    query.select(fields);
+  }
+
+  // Sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query.sort(sortBy);
+  } else {
+    query.sort('-createdAt');
+  }
+
+  // Pagination
+  const limit = parseInt(req.query.limit, 10) || 25;
+  const page = parseInt(req.query.page, 10) || 1;
+  const startIdx = (page - 1) * limit;
+  const endIdx = page * limit;
+  const total = await Bootcamp.countDocuments();
+  query.skip(startIdx).limit(limit);
+
+  // Executing query
+  const bootcamps = await query;
+
+  // Pagination result
+  const pagination = {};
+
+  if (endIdx < total) {
+    pagination.next = {
+      page: page + 1,
+      limit,
+    };
+  }
+
+  if (startIdx > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit,
+    };
+  }
+
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    pagination,
+    data: bootcamps,
+  });
 });
 
 // @desc    Get single bootcamp
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-export const getBootcamp = asyncHandler(async (req, res, next) => {  
-    const bootcamp = await Bootcamp.findById(req.params.id);
+export const getBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findById(req.params.id);
 
-    if (!bootcamp) {
-      return next(new ErrorResponse(`Bootcamp that ends with '${req.params.id.slice(-6)}' not found`, 404));
-    }
+  if (!bootcamp) {
+    return next(new ErrorResponse(`Bootcamp that ends with '${req.params.id.slice(-6)}' not found`, 404));
+  }
 
   res.status(200).json({ success: true, data: bootcamp });
 });
@@ -31,27 +91,27 @@ export const getBootcamp = asyncHandler(async (req, res, next) => {
 // @desc    Create a bootcamp
 // @route   POST /api/v1/bootcamps
 // @access  Private
-export const createBootcamp = asyncHandler(async (req, res, next) => {  
-    const bootcamp = await Bootcamp.create(req.body);
+export const createBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.create(req.body);
 
-    res.status(201).json({
-      success: true,
-      data: bootcamp,
-    });
+  res.status(201).json({
+    success: true,
+    data: bootcamp,
+  });
 });
 
 // @desc    Update a bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
-export const updateBootcamp = asyncHandler(async (req, res, next) => {  
-    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+export const updateBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
-    if (!bootcamp) {
-      return next(new ErrorResponse(`Bootcamp that ends with '${req.params.id.slice(-6)}' not found`, 404));
-    }
+  if (!bootcamp) {
+    return next(new ErrorResponse(`Bootcamp that ends with '${req.params.id.slice(-6)}' not found`, 404));
+  }
 
   res.status(200).json({ success: true, data: bootcamp });
 });
@@ -59,12 +119,39 @@ export const updateBootcamp = asyncHandler(async (req, res, next) => {
 // @desc    Delete a bootcamp
 // @route   DELETE /api/v1/bootcamps/:id
 // @access  Private
-export const deleteBootcamp = asyncHandler(async (req, res, next) => {  
-    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+export const deleteBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
 
-    if (!bootcamp) {
-      return next(new ErrorResponse(`Bootcamp that ends with '${req.params.id.slice(-6)}' not found`, 404));
-    }
+  if (!bootcamp) {
+    return next(new ErrorResponse(`Bootcamp that ends with '${req.params.id.slice(-6)}' not found`, 404));
+  }
 
   res.status(200).json({ success: true, data: {} });
+});
+
+// @desc    Get bootcamps within a radius
+// @route   GET /api/v1/bootcamps/radius/:zipcode/:distance
+// @access  Public
+export const getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+  const { zipcode, distance } = req.params;
+
+  // Get lat/lng from geocoder
+  const loc = await geocoder.geocode(zipcode);
+  const lat = loc[0].latitude;
+  const lng = loc[0].longitude;
+
+  // Calc radius using radians
+  // Divide distance by radius of Earth
+  // Earth Radius = 6,378.1 km
+  const radius = distance / 6378.1;
+
+  const bootcamps = await Bootcamp.find({
+    location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+  });
+
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    data: bootcamps,
+  });
 });
