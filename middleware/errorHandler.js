fcb19a6362df335dab168cdd10123a2c73653394
Ori Baram <obrm770@gmail.com>
2024-02-19 16:38:00 +0200

    refactor errorHandler

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -5,25 +5,37 @@ const errorHandler = (error, req, res, next) => {
 
   err.message = error.message;
 
-  console.log(error.stack.red);
+  if (process.env.NODE_ENV === 'development') {
+		console.log(error.stack.red);
+	}
 
-  // Mongoose bad ObjectId
-  if (error.name === "CastError") {
-    const message = `Resource not found`;
-    err = new ErrorResponse(message, 404);
-  }
+	// Mongoose bad ObjectId
+	if (error.name === 'CastError') {
+		const message = `Resource not found`;
+		err = new ErrorResponse(message, 404);
+	}
 
-  // Mongoose duplicate key
-  if (error.code === 11000) {
-    const message = "Duplicate field value entered";
-    err = new ErrorResponse(message, 400);
-  }
+	// Mongoose duplicate key
+	if (error.code === 11000) {
+		const message =
+			'Duplicate field value entered';
+		err = new ErrorResponse(message, 400);
+	}
 
-  // Mongoose validation error
-  if (error.name === 'ValidationError') {
-    const message = Object.values(err.errors).map(val => val.message).join(', ');
-    err = new ErrorResponse(message, 400);
-  }
+	// Mongoose validation error
+	if (error.name === 'ValidationError') {
+		const message = Object.values(err.errors)
+			.map((val) => val.message)
+			.join(', ');
+		err = new ErrorResponse(message, 400);
+	}
+
+	if (!(error instanceof ErrorResponse)) {
+		const statusCode = error.statusCode || 500;
+		const message =
+			error.message || 'Internal Server Error';
+		err = new ErrorResponse(message, statusCode);
+	}
 
   res.status(err.statusCode || 500).json({
     success: false,
